e4c3beb90be7390298c1488007c0a45eb62e3f49
Umar Khashimov <umarkhashimov05@gmail.com>
2023-04-17 00:19:49 +0500

    full working chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ function App() {
   const Behaviour = 'Explain like I am 10 years old.'
 
   const [text, setText] = useState('')
-  const [respond, setRespond] = useState(null)
   const [status, setStatus] = useState(false)
+  const [AllMessages, SetAllMessages] = useState({ messages: [] })
 
-  function setValue(inp) {
-    setText(inp.target.value)
+  const setNewMessage = (messages) => {
+    SetAllMessages({ messages })
+    window.scrollTo(0, document.body.scrollHeight);
   }
 
   const systemMessage = {
@@ -19,42 +20,63 @@ function App() {
     content: Behaviour
   }
 
-  async function requestRespond() {
-    setRespond(null)
-    setStatus(true)
+  const requestBody = {
+    'model': "gpt-3.5-turbo",
+    'messages': [
+      systemMessage,
+      ...AllMessages.messages
+    ]
+  }
 
+  async function requestRespond() {
+    setStatus(true)
     await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         'Authorization': 'Bearer ' + API_KEY,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(
-        {
-        'model': "gpt-3.5-turbo",
-        'messages': [
-          systemMessage, 
-          { role: 'user', content: text}
-        ]
-      })
+      body: JSON.stringify(requestBody)
     }).then((data) => {
       return data.json();
     }).then((data) => {
       setStatus(false)
-      setRespond(data.choices[0].message.content)
-      console.log(data.choices[0].message.content)
+      const newData = {
+        role: 'assistant',
+        content: data.choices[0].message.content
+      }
+      const { messages } = AllMessages
+      messages.push(newData)
+      setNewMessage(messages)
+      console.log(AllMessages)
     })
+  }
+
+  const Request = () => {
+    const newData = {
+      role: 'user',
+      content: text
+    }
+
+    const { messages } = AllMessages
+    messages.push(newData)
+    setNewMessage(messages)
+    requestRespond()
   }
 
   return (
     <div className="App">
-      <div className='form'>
-        <input value={text.content} type="text" placeholder='Enter the question ...' className='question' name="" onChange={setValue} id="" />
-        <button onClick={requestRespond}>Submit</button>
-      </div>
       <div className='respond'>
-        {status ? <p className='loader-div'><span className='loader'></span></p> : ''}
-        {respond ? <p>{respond}</p> : '' }
+        {AllMessages.messages.length > 0 ? AllMessages.messages.map((message) => (
+          <p className={message.role}>{message.content}</p>
+        )) :
+          <p className='no-message'>No messages yet</p>
+        }
+        {status ? <p className='loader-div system'><span className='loader'></span></p> : ''}
+      </div>
+      <div className='form'>
+        <input value={text.content} type="text" placeholder='Enter the question ...' className='question' name="" onChange={e => { setText(e.target.value) }} id="" />
+        <button onClick={Request}>Submit</button>
       </div>
     </div>
   )
